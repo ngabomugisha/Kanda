5178d76ecb9a15ba60c2d0ecb2d1ddd2ad6a4ccb
RRdD <rd@RDs-MacBook-Pro.local>
2021-01-12 18:00:53 +0200

    first push

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View, Alert, Image, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback } from 'react-native';
 
 export default function App() {
+  let rob = 'rwandan';
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button title="Click Me" width={100} color="orange" onPress={()=> Alert.alert("myTitle","robert pressed a button")}/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
